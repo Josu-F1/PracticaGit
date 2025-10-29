224d982128b8196a261499256764274c59dff746
JOSU <jfiallos7065@uta.edu.ec>
2025-10-29 18:43:38 -0500

    Revert "otra prueba"
    This reverts commit 6831f9f1bcbb4db1c59d157c0b3edac200c545ed.

--- src/practicargit/PracticarGit.java
+++ src/practicargit/PracticarGit.java
@@ -15,7 +15,6 @@ public class PracticarGit {
      */
     public static void main(String[] args) {
         System.out.println("hola josu");
-        System.out.println("otra prueba");
     }
     
 }
