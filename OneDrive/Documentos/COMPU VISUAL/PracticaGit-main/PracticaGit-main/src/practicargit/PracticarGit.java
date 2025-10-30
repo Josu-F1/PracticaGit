04ba22756bad789b5c9a978ecbdc7a863c76077e
BryanLopez257 <blopez2242@uta.edu.ec>
2025-10-29 21:47:03 -0500

    Mi primer commit BL

---
+++ OneDrive/Documentos/COMPU VISUAL/PracticaGit-main/PracticaGit-main/src/practicargit/PracticarGit.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practicargit;
+
+/**
+ *
+ * @author ASUS
+ */
+public class PracticarGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hola josu");
+        
+        System.out.println("otra prueba");
+        
+        System.out.println("hola¡¡¡¡¡¡");
+        
+        System.out.println(" para ver que pasa");
+        
+        
+        System.out.println("Hola Pablo");
+        
+        System.out.println("Holaaa, aqui Bryan :3");
+    }
+    
+}
